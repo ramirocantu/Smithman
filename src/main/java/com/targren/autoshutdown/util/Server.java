8322ecbcf17e6d7e66b9359cc3bbbc0788d42cbd
Targren <git.geekciro@spamgourmet.com>
2018-12-21 19:26:30 -0500

    Initial Commit
    Based on 1.10 mod by polaris_iv/abused_master
    
    Ported to Minecraft 1.12.2 + Forge Version 14.23.5.2768
    
    Minor adjustments to default config values.

---
+++ src/main/java/com/targren/autoshutdown/util/Server.java
@@ -0,0 +1,48 @@
+package com.targren.autoshutdown.util;
+
+import com.targren.autoshutdown.ForgeAutoShutdown;
+import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.TextComponentString;
+import net.minecraftforge.common.util.FakePlayer;
+import org.apache.logging.log4j.Logger;
+
+/**
+ * Static utility class for server functions
+ */
+public class Server
+{
+    // Safe to declare here. This class should only ever be loaded if any of its methods
+    // are called, which by then these should be available.
+    static final MinecraftServer SERVER = ForgeAutoShutdown.server;
+    static final Logger          LOGGER = ForgeAutoShutdown.LOGGER;
+
+    /** Kicks all players from the server with given reason, then shuts server down */
+    public static void shutdown(String reason)
+    {
+        reason = Chat.translate(reason);
+
+        //for ( Object value : SERVER.getPlayerList().getPlayerList().toArray() )
+        for ( Object value : SERVER.getPlayerList().getPlayers().toArray() )
+        {
+            EntityPlayerMP player = (EntityPlayerMP) value;
+            ITextComponent message = new TextComponentString(reason);
+            player.connection.disconnect(message);
+            //player.connection.kickPlayerFromServer(reason);
+        }
+
+        LOGGER.debug("Shutdown initiated because: %s", reason);
+        SERVER.initiateShutdown();
+    }
+
+    /** Checks if any non-fake player is present on the server */
+    public static boolean hasRealPlayers()
+    {
+        for ( Object value : SERVER.getPlayerList().getPlayers().toArray() )
+            if (value instanceof EntityPlayerMP)
+            if ( !(value instanceof FakePlayer) )
+                return true;
+        return false;
+    }
+}

bb3f3ddf92e7f5394ecda90e75cfdcf1c7320db7
Targren <gitlab.targren@0sg.net>
2018-12-22 01:14:20 -0500

    Changed MODID to correspond with the curseforge-demanded name change.

--- src/main/java/com/targren/autoshutdown/ForgeAutoShutdown.java
+++ src/main/java/com/targren/autoshutdown/ForgeAutoShutdown.java
@@ -22,7 +22,7 @@ import org.apache.logging.log4j.Logger;
 public class ForgeAutoShutdown
 {
     public static final String VERSION = "1.12.2-0.0.1";
-    public static final String MODID   = "forgeautoshutdown";
+    public static final String MODID   = "s3scheduledservershutdown";
     public static final Logger LOGGER  = LogManager.getFormatterLogger(MODID);
 
     public static MinecraftServer server;
